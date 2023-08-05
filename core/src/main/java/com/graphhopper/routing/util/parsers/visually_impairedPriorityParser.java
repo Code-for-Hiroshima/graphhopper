d22377d05bc4da06813ee3ab2f3743a478926353
Masataka Shinke <mshinke@users.sourceforge.jp>
2023-08-06 02:59:58 +0900

    Set priority of "footway=crossing" tag

--- core/src/main/java/com/graphhopper/routing/util/parsers/visually_impairedPriorityParser.java
+++ core/src/main/java/com/graphhopper/routing/util/parsers/visually_impairedPriorityParser.java
@@ -6,12 +6,17 @@ import com.graphhopper.routing.util.PriorityCode;
 import com.graphhopper.storage.IntsRef;
 import com.graphhopper.util.PMap;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.TreeMap;
 
+import static com.graphhopper.routing.util.PriorityCode.REACH_DESTINATION;
 import static com.graphhopper.routing.util.PriorityCode.AVOID;
 import static com.graphhopper.routing.util.PriorityCode.VERY_NICE;
 
 public class visually_impairedPriorityParser extends FootPriorityParser {
+    private boolean traffic_signals = false;
 
     public visually_impairedPriorityParser(EncodedValueLookup lookup, PMap properties) {
         this(lookup.getDecimalEncodedValue(properties.getString("name", VehiclePriority.key("visually_impaired"))),
@@ -47,6 +52,33 @@ public class visually_impairedPriorityParser extends FootPriorityParser {
     @Override
     public int handlePriority(ReaderWay way, Integer priorityFromRelation) {
         TreeMap<Double, Integer> weightToPrioMap = new TreeMap<>();
+
+        traffic_signals=false;
+        Arrays.asList(
+            "sound",
+            "vibration",
+            "arrow",
+            "minimap",
+            "floor_vibration",
+            "countdown",
+            "floor_light"
+        ).forEach(s -> {
+            if(way.hasTag("traffic_signals:"+s,"yes"))
+                traffic_signals=true;
+        });
+        Set<String> tacttile_paving = new HashSet<>();
+        tacttile_paving.add("yes");
+        tacttile_paving.add("contrasted");
+        tacttile_paving.add("primitive");
+
+        if( ! (traffic_signals || way.hasTag("tactile_paving",tacttile_paving) )){
+            Set<String> footway = new HashSet<>();
+            footway.add("crossing");
+            if (way.hasTag("footway", footway)){
+                weightToPrioMap.put(100d, REACH_DESTINATION.getValue());
+                return weightToPrioMap.lastEntry().getValue();
+            }
+        };
 
         weightToPrioMap.put(100d, super.handlePriority(way, priorityFromRelation));
 
