de660d9f7be8a91142e9251ef79966067d221134
Masataka Shinke <masataka@shinke.com>
2023-11-06 04:12:18 +0900

    tactile_paving_8.0

--- core/src/main/java/com/graphhopper/routing/util/VehicleEncodedValues.java
+++ core/src/main/java/com/graphhopper/routing/util/VehicleEncodedValues.java
@@ -27,7 +27,7 @@ import java.util.List;
 import static com.graphhopper.routing.util.VehicleEncodedValuesFactory.*;
 
 public class VehicleEncodedValues {
-    public static final List<String> OUTDOOR_VEHICLES = Arrays.asList(BIKE, RACINGBIKE, MOUNTAINBIKE, FOOT, WHEELCHAIR);
+    public static final List<String> OUTDOOR_VEHICLES = Arrays.asList(BIKE, RACINGBIKE, MOUNTAINBIKE, FOOT, WHEELCHAIR , VISUALLY_IMPAIRED);
 
     private final String name;
     private final BooleanEncodedValue accessEnc;
@@ -55,6 +55,19 @@ public class VehicleEncodedValues {
                 .putObject("name", properties.getString("name", "wheelchair"))
                 .putObject("speed_two_directions", true)
         );
+    }
+
+    public static VehicleEncodedValues visually_impaired(PMap properties) {
+        String name = properties.getString("name", "visually_impaired");
+        int speedBits = properties.getInt("speed_bits", 4);
+        double speedFactor = properties.getDouble("speed_factor", 1);
+        boolean speedTwoDirections = properties.getBool("speed_two_directions", false);
+        boolean turnCosts = properties.getBool("turn_costs", false);
+        BooleanEncodedValue accessEnc = VehicleAccess.create(name);
+        DecimalEncodedValue speedEnc = VehicleSpeed.create(name, speedBits, speedFactor, speedTwoDirections);
+        DecimalEncodedValue priorityEnc = VehiclePriority.create(name, 4, PriorityCode.getFactor(1), false);
+        BooleanEncodedValue turnRestrictionEnc = turnCosts ? TurnRestriction.create(name) : null;
+        return new VehicleEncodedValues(name, accessEnc, speedEnc, priorityEnc, turnRestrictionEnc);
     }
 
     public static VehicleEncodedValues bike(PMap properties) {
